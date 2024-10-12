c14bb8360eb17b17a190795d990e4a11cf12a98c
rookie <rookie@test.com>
2024-10-12 18:09:10 +0800

    sdk版本修改

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.a68opendemo"
-        minSdk = 33
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
